9c35a42518e94fe8f6f733814fdf1da03664d8ee
Félix Paquet <felix.paquet.6@ulaval.ca>
2026-01-17 11:44:47 -0500

    C'est un début I guess

--- bot.py
+++ bot.py
@@ -19,33 +19,22 @@ class Bot:
 
         if len(my_team.spawners) == 0:
             actions.append(SporeCreateSpawnerAction(sporeId=my_team.spores[0].id))
-        elif len(my_team.spores) == 0:
-            print("No spores left to command!")
+        elif my_team.nutrients > 10:
             actions.append(SpawnerProduceSporeAction(spawnerId=my_team.spawners[0].id, biomass=5))
         else:
             for action in self.moveAllSporesTo(my_team.spores, self.fillSpawnerZone(my_team.spawners[0], game_message)):
                 actions.append(action)
+                print(action.position.x, action.position.y)
 
-
+        return actions
         # You can clearly do better than the random actions above. Have fun!!
         return actions
-
-    def getHighValueNutrimentInZone(self, game_message: TeamGameState, spawner: Spawner) -> list[tuple[int, int]]:
-        """
-        Returns a list of coordinates with high nutrient levels.
-        """
-        high_value_coords = []
-        for y in range(game_message.world.map.height):
-            for x in range(game_message.world.map.width):
-                if game_message.world.map.nutrientGrid[y][x] > 50:
-                    high_value_coords.append((x, y))
-        return high_value_coords
 
     def fillSpawnerZone(self, spawner: Spawner, game_message: TeamGameState) -> list[Position]:
         """
         Calculates the Manhattan distance between two positions.
         """
-        zone_coords = [Position]
+        zone_coords: list[Position] = []
         for y in range(max(0, spawner.position.y - 5), min(game_message.world.map.height, spawner.position.y + 5)):
             for x in range(max(0, spawner.position.x - 5), min(game_message.world.map.width, spawner.position.x + 5)):
                 zone_coords.append(Position(x=x, y=y))
@@ -56,10 +45,9 @@ class Bot:
         Generates move actions for all spores to a target coordinate.
         """
         actions = []
-        index = 0
         for spore in spores:
-            actions.append(SporeMoveToAction(sporeId=spore.id, position=targets[index]))
-            index += 1
-            if index >= len(targets):
+            actions.append(SporeMoveToAction(sporeId=spore.id, position=targets[0]))
+            targets.pop(0)
+            if len(targets) == 0:
                 return actions
         return actions
